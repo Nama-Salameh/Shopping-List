e605287ee7d736245e79d0c8e9ceaf335c6526e8
Nama' Salameh <nama.salameh@gmail.com>
2023-10-30 16:20:49 +0200

    Change the icon and name in the header.

--- Shopping_List_FrontEnd/src/components/ShoppingListHeader/ShoppingList.jsx
+++ Shopping_List_FrontEnd/src/components/ShoppingListHeader/ShoppingList.jsx
@@ -1,11 +1,15 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import style from "./ShoppingList.module.css";
+import myImage from '../../assets/perfume_logo.jpeg';
+
 const ShoppibgListHeader = () => {
   return (
     <div className={style.headerContainer}>
-      <div className={style.h1Container}><img src="../../cart.png" className={style.logo}></img>
-      <h1>Shopping List</h1> </div>
-      <ShoppingCartIcon  fontSize="large"/>
+      <div className={style.h1Container}>
+        <img src={myImage} className={style.logo} alt="cart"></img>
+        <h1>Perfumes</h1>
+      </div>
+      <ShoppingCartIcon fontSize="large" />
     </div>
   );
 };
