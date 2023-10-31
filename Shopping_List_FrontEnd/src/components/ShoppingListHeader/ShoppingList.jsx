208f6798db9cf8b6d6019c6d543cd7051d631e4e
Nama' Salameh <nama.salameh@gmail.com>
2023-10-31 15:35:38 +0200

    Change the logo.

--- Shopping_List_FrontEnd/src/components/ShoppingListHeader/ShoppingList.jsx
+++ Shopping_List_FrontEnd/src/components/ShoppingListHeader/ShoppingList.jsx
@@ -1,12 +1,12 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import style from "./ShoppingList.module.css";
-import myImage from '../../assets/perfume_logo.jpeg';
+import logo from '../../assets/perfume_logo.jpeg';
 
 const ShoppibgListHeader = () => {
   return (
     <div className={style.headerContainer}>
       <div className={style.h1Container}>
-        <img src={myImage} className={style.logo} alt="cart"></img>
+        <img src={logo} className={style.logo} alt="cart"></img>
         <h1>Perfumes</h1>
       </div>
       <ShoppingCartIcon fontSize="large" />
