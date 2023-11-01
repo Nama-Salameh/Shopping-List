3c37c4fc70137058e91b77b6dcdc687549f98fe0
Nama' Salameh <nama.salameh@gmail.com>
2023-11-01 11:42:19 +0200

    Add cart items table to the database.

--- Shopping_List_Backend/database.js
+++ Shopping_List_Backend/database.js
@@ -1,9 +1,9 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("items.db");
-const fs = require('fs');
+const fs = require("fs");
 
 db.run("DELETE FROM items", (err) => {
-    console.log("delete items")
+  console.log("delete items");
   if (err) {
     res.status(500).json({ error: err.message });
     return;
@@ -25,29 +25,44 @@ db.run(
     } else {
       console.log("Items table created successfully.");
       populateDatabase();
-
     }
   }
 );
 
+db.run(
+  `
+CREATE TABLE IF NOT EXISTS cart_items (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  image TEXT,
+  name TEXT,
+  price REAL
+)
+`,
+  (err) => {
+    if (err) {
+      console.error("Error creating cart_items table:", err);
+    } else {
+      console.log("Cart items table created successfully.");
+    }
+  }
+);
 
-// Populate the database with data from data.json
 function populateDatabase() {
-    const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-    
-    // Insert data into the database
-    data.forEach((item) => {
-      const { itemImage, itemName, itemPrice } = item;
-    
-      // Read the image file as binary data
-      //const imageBuffer = fs.readFileSync(itemImage, null);
-    
-      db.run('INSERT INTO items (image, name, price) VALUES (?, ?, ?)', [itemImage, itemName, itemPrice], (err) => {
+  const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+  data.forEach((item) => {
+    const { itemImage, itemName, itemPrice } = item;
+    db.run(
+      "INSERT INTO items (image, name, price) VALUES (?, ?, ?)",
+      [itemImage, itemName, itemPrice],
+      (err) => {
         if (err) {
-          console.error('Error inserting item:', err);
+          console.error("Error inserting item:", err);
         } else {
-          console.log('Item inserted successfully.');
+          console.log("Item inserted successfully.");
         }
-      });
-    });}
+      }
+    );
+  });
+}
+
 module.exports = db;
