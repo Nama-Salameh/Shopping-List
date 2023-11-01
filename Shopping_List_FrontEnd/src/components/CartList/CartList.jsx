10a676fdbfa4caf894b29ffa1d2a1a6d5228069b
Nama' Salameh <nama.salameh@gmail.com>
2023-11-01 14:44:53 +0200

    Add Dialog to enable users enter name & email.

--- Shopping_List_FrontEnd/src/components/CartList/CartList.jsx
+++ Shopping_List_FrontEnd/src/components/CartList/CartList.jsx
@@ -1,10 +1,33 @@
 import style from "./CartList.module.css";
 import { useCart } from "./CartContext";
 import Cart from "./Cart";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import CheckoutDialog from "./CheckoutDialog";
 
 const CartList = () => {
+  const [isOpenCheckoutDialog, setIsOpenCheckoutDialog] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const { cartItems } = useCart();
   let totalAmount = 0;
+
+  const handlecheckoutClick = () => {
+    setIsOpenCheckoutDialog(true);
+    setName("");
+    setEmail("");
+  };
+  const handleCheckoutDialogClose = () => {
+    setIsOpenCheckoutDialog(false);
+  };
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   if (!cartItems || cartItems.length === 0) {
     return (
       <div>
@@ -20,7 +43,23 @@ const CartList = () => {
         totalAmount += cartItem.price;
         return <Cart key={index} cart={cartItem} />;
       })}
-      <h4>Total Amount : {totalAmount} </h4>
+      <div className={style.footerContainer}>
+        <h3>Total Amount : {totalAmount} </h3>
+        <Button className={style.checkoutButton} onClick={handlecheckoutClick}>
+          Checkout
+        </Button>
+      </div>
+
+      {isOpenCheckoutDialog && (
+        <CheckoutDialog
+          isOpenCheckoutDialog={isOpenCheckoutDialog}
+          handleCheckoutDialogClose={handleCheckoutDialogClose}
+          name={name}
+          email={email}
+          handleNameChange={handleNameChange}
+          handleEmailChange={handleEmailChange}
+        />
+      )}
     </div>
   );
 };
