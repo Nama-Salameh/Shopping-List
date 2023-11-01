c4edf83deefb7fabbd48c80255597c00317b8fb4
Nama' Salameh <nama.salameh@gmail.com>
2023-11-01 11:46:52 +0200

    Adding Routing for cartIcon.

--- Shopping_List_FrontEnd/src/components/ShoppingListHeader/ShoppingList.jsx
+++ Shopping_List_FrontEnd/src/components/ShoppingListHeader/ShoppingList.jsx
@@ -1,16 +1,23 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import style from "./ShoppingList.module.css";
-import logo from '../../assets/perfume_logo.jpeg';
+import logo from "../../assets/perfume_logo.jpeg";
+import { Link } from "react-router-dom";
+import { useCart } from "../CartList/CartContext";
 
-const ShoppibgListHeader = () => {
+const ShoppingListHeader = () => {
+  const { cartItems } = useCart();
+
   return (
     <div className={style.headerContainer}>
       <div className={style.h1Container}>
-        <img src={logo} className={style.logo} alt="cart"></img>
+        <img src={logo} className={style.logo} alt="perfumes logo" />
         <h1>Perfumes</h1>
       </div>
-      <ShoppingCartIcon fontSize="large" />
+      <Link to="/cart">
+        <ShoppingCartIcon fontSize="large" />
+        <span>{cartItems.length}</span>
+      </Link>
     </div>
   );
 };
-export default ShoppibgListHeader;
+export default ShoppingListHeader;
